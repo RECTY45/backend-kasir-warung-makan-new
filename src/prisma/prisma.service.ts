bccaac2cb44d68a41229b3fbd7be49bbb9dec053
RECTY45 <rectyexploit@gmail.com>
2026-07-23 00:35:06 +0800

    Fix: Remove PrismaMariaDb adapter to connect natively to MySQL/MariaDB

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,8 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
-    super({ adapter });
-  }
-
   async onModuleInit() {
     await this.$connect();
   }
